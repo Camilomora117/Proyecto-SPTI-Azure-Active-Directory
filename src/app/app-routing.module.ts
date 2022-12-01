dfdab97d05132b8b2a0d68ed8465b52849ffd840
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-11-30 19:43:56 -0500

    Ultimos cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PantallaPrincipalComponent } from './Views/pantalla-principal/pantalla-principal.component'
-import { CrearSubastaComponent } from './Views/crear-subasta/crear-subasta.component'
-import{ MisSubastasComponent } from './Views/mis-subastas/mis-subastas.component'
-import { DetalleSubastaComponent } from './Views/detalle-subasta/detalle-subasta.component'
 import { BrowserUtils } from '@azure/msal-browser';
 import { ProfileComponent } from './profile/profile.component';
 import { MsalGuard } from '@azure/msal-angular';
 
 
 const routes: Routes = [
-  {path: '', component: PantallaPrincipalComponent},
-  {path: 'pantalla-principal', component: PantallaPrincipalComponent},
-  {path: 'crear-subasta', component: CrearSubastaComponent },
-  {path: 'mis-subastas', component: MisSubastasComponent},
-  {path: 'detalle-subasta', component: DetalleSubastaComponent},
-  {path: 'profile', component: ProfileComponent, canActivate:[MsalGuard]}
 ];
 
 const isIframe = window !== window.parent && !window.opener;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PantallaPrincipalComponent } from './Views/pantalla-principal/pantalla-principal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import { MatIconModule } from '@angular/material/icon';
-import { CrearSubastaComponent } from './Views/crear-subasta/crear-subasta.component';
 import { FormsModule } from '@angular/forms';
-import { MisSubastasComponent } from './Views/mis-subastas/mis-subastas.component';
-import { DetalleSubastaComponent } from './Views/detalle-subasta/detalle-subasta.component'; 
 
 import { MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent } from '@azure/msal-angular'
 import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
 import { ProfileComponent } from './profile/profile.component';
-import { PujaSubastaComponent } from './Views/puja-subasta/puja-subasta.component';
 import { AzureAdDemoService } from './azure-ad-demo.service';
 
 const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
@@ -25,12 +20,7 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
 @NgModule({
   declarations: [
     AppComponent,
-    PantallaPrincipalComponent,
-    CrearSubastaComponent,
-    MisSubastasComponent,
-    DetalleSubastaComponent,
     ProfileComponent,
-    PujaSubastaComponent
   ],
   imports: [
     BrowserModule,
@@ -42,8 +32,8 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
     HttpClientModule,
     MsalModule.forRoot(new PublicClientApplication({
       auth: {
-        clientId:'48b81b58-149e-4a2b-8e7e-070c0586a1b8',
-        authority: 'https://login.microsoftonline.com/adad22ad-d3c9-4783-a7c8-9aa2777bf35a',
+        clientId:'4decc0c0-8022-4698-bc7e-6d2a498d6f5b',
+        authority: 'https://login.microsoftonline.com/368e69d2-6e39-4f7d-a4c2-c1af88eeab75',
         redirectUri: 'http://localhost:4200'
       },
       cache: {
